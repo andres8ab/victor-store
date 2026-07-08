09cae1e5fe8c9b05784ff0c3e6e8f0a0585cc1b0
Andres Ochoa <ochoabawab@gmail.com>
2026-07-08 12:50:15 -0500

    added promobar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./Navbar";
+export { default as PromoBar } from "./PromoBar";
 export { default as Card } from "./Card";
 export { default as Footer } from "./Footer";
 export { default as AuthForm } from "./AuthForm";
